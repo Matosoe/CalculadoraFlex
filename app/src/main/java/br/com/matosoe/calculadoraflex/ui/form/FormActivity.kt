d4944f52a5b322193608fded83f2aca648b67bbc
Logon Aluno <logonaluno@local.com>
2019-08-17 11:02:01 -0300

    Initial commit

---
+++ app/src/main/java/br/com/matosoe/calculadoraflex/ui/form/FormActivity.kt
@@ -0,0 +1,13 @@
+package br.com.matosoe.calculadoraflex.ui.form
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.matosoe.calculadoraflex.R
+
+class FormActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_form)
+    }
+}
